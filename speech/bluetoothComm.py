97f3757f1db7a6be66ca542e999d8a8c9ecc4af0
Sudip-Thakur <109968647+Sudip-Thakur@users.noreply.github.com>
2023-01-22 07:23:23 +0545

    Update bluetoothComm.py

--- speech/bluetoothComm.py
+++ speech/bluetoothComm.py
@@ -3,40 +3,16 @@ import time
 import os
 
 
-port="/dev/tty.HC-06-DevB" 
-bluetooth=serial.Serial(port, 9600)
+USB="/dev/ttyACM0"
+BT="/dev/rfcomm0"
+
+
+bluetooth=serial.Serial(BT, 9600)
 print("Connected")
 
 #bluetooth.flushInput() 
 
-signs={
- "1":"A",
-"2":"B",
-"3":"C",
-"4":"D",
-"5":"E",
-"6":"F",
-"7":"G",
-"8":"H",
-"9":"I",
-"10":"J",
-"11":"K",
-"12":"L",
-"13":"M",
-"14":"N",
-"15":"O",
-"16":"P",
-"17":"Q",
-"18":"R",
-"19":"S",
-"20":"T",
-"21":"U",
-"22":"V",
-"23":"W",
-"24":"X",
-"25":"Y",
-"26":"Z"
-}
+signs=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'hello', ]
 
 #os.system("espeak {}".format("\"sudip is a boy\""))
 
@@ -45,9 +21,9 @@ signs={
 
 while(1):
     input_data=bluetooth.readline()
-    x=int(input_data.decode())
+    x=(int(input_data.decode()))
 
-    os.system(f"espeak \"{x}\"")
+    os.system(f"espeak \"{signs[x]}\"")
 
  
 print("Done")
