cb5cd13347318c1c9ac56f4f706e0a35209ea29d
Sudip-Thakur <109968647+Sudip-Thakur@users.noreply.github.com>
2023-01-22 14:49:06 +0545

    Update btTest.py

--- speech/btTest.py
+++ speech/btTest.py
@@ -2,18 +2,54 @@ import serial
 import os
 import keyboard
 
-x=serial.Serial("/dev/ttyACM0",9600,timeout=1)
+
+USB="/dev/ttyACM0"
+BT="/dev/rfcomm0"
+
+x=serial.Serial(BT,9600,timeout=1)
+
+
+# class Sign:
+#     def __init__(self,bcd,sign):
+#         self.played=0
+#         self.bcd=bcd
+#         self.sign=sign
+    
+#     def setPlayed(self,x):
+#         self.played=x
+
+signs=['A','E','I','O','U','B','S','D','J','N','H','P','K','hello', 'world','i love you','good morning']
+
+# signObjs=[Sign(s) for s in signs]
 
 played=0
+y=0
 
 while(1):
     try:
-        y=(float(x.readline().decode()))
-        if(y<0.5 and played==0):
-            keyboard.press_and_release("right")
-            played=1
-        if(y>0.5):
-            played=0
+        prev=y
+        y=(int(x.readline().decode()))
+        print(y)
+        # if(y<0.5 and played==0):
+        #     keyboard.press_and_release("right")
+        #     played=1
+        # if(y>0.5):
+        #     played=0
+
+        if(y==69420 or y==42069):
+            if(y==69420):
+                keyboard.press_and_release("right")
+            if(y==42069):
+                keyboard.press_and_release("left")
+
+        else:
+            if(played==0):
+                os.system(f"espeak \"{signs[y]}\"")
+                played=1
+        
+            if(prev!=y):
+                played=0
+        
 
     except Exception as e:
         print(e)
